7aef882f18937d4f28d6741a9d8d1613a06456a4
Carsten Griesheimer <sun_od@hotmail.com>
2012-06-16 20:25:57 +0200

    +proxy server

--- em-server/server.rb
+++ em-server/server.rb
@@ -1,11 +1,11 @@
 require 'em-websocket'
 require 'json'
 
+@host = "172.31.8.50"
+@port = 8080
+
+
 @playlist = []
-clients = []
-
-#@playlist = '[{"label":"10. The Black Keys - Nova Baby","artist":"The Black Keys","album":"El Camino","year":2011,"genre":"Alternative","thumbnail":"http://172.31.8.13:8090/vfs/special://masterprofile/Thumbnails/Music/a/ad1321ee.tbn","title":"Nova Baby"},{"label":"05. A Tribe Called Quest - Crew","artist":"A Tribe Called Quest","album":"Beats, Rhymes & Life","year":1996,"genre":"Hip-Hop/Rap","thumbnail":"http://172.31.8.13:8090/vfs/special://masterprofile/Thumbnails/Music/5/5a5b82eb.tbn","title":"Crew"},{"label":"03. Bahamas - Montreal","artist":"Bahamas","album":"Barchords","year":2012,"genre":"Pop","thumbnail":"http://172.31.8.13:8090/vfs/special://masterprofile/Thumbnails/Music/5/5e2be68d.tbn","title":"Montreal"},{"label":"04. Balkan Brass Band - Varnensko horo","artist":"Balkan Brass Band","album":"Balkan Brass Band","year":2009,"genre":"Weltmusik","thumbnail":"http://172.31.8.13:8090/vfs/special://masterprofile/Thumbnails/Music/c/c0e6d961.tbn","title":"Varnensko horo"},{"label":"03. Electric Guest - This Head I Hold","artist":"Electric Guest","album":"American Daydream - EP","year":2012,"genre":"Alternative","thumbnail":"http://172.31.8.13:8090/vfs/special://masterprofile/Thumbnails/Music/7/709dacee.tbn","title":"This Head I Hold"},{"label":"02. Peaches - AA XXX","artist":"Peaches","album":"The Teaches of Peaches","year":2000,"genre":"Electronic","thumbnail":"http://172.31.8.13:8090/vfs/special://masterprofile/Thumbnails/Music/f/f2344293.tbn","title":"AA XXX"}]'
-
 
 @playlist.push({
   label: "10. The Black Keys - Nova Baby",
@@ -15,47 +15,73 @@ clients = []
   genre: "Alternative",
   thumbnail: "http://172.31.8.13:8090/vfs/special://masterprofile/Thumbnails/Music/a/ad1321ee.tbn",
   title:"Nova Baby"
-  })
+})
 
-def onOpen(ws)
-  #clients.push(ws)
-  #ws.send '
+# @i = 0
+# #@playlist = '[{"label":"10. The Black Keys - Nova Baby","artist":"The Black Keys","album":"El Camino","year":2011,"genre":"Alternative","thumbnail":"http://172.31.8.13:8090/vfs/special://masterprofile/Thumbnails/Music/a/ad1321ee.tbn","title":"Nova Baby"},{"label":"05. A Tribe Called Quest - Crew","artist":"A Tribe Called Quest","album":"Beats, Rhymes & Life","year":1996,"genre":"Hip-Hop/Rap","thumbnail":"http://172.31.8.13:8090/vfs/special://masterprofile/Thumbnails/Music/5/5a5b82eb.tbn","title":"Crew"},{"label":"03. Bahamas - Montreal","artist":"Bahamas","album":"Barchords","year":2012,"genre":"Pop","thumbnail":"http://172.31.8.13:8090/vfs/special://masterprofile/Thumbnails/Music/5/5e2be68d.tbn","title":"Montreal"},{"label":"04. Balkan Brass Band - Varnensko horo","artist":"Balkan Brass Band","album":"Balkan Brass Band","year":2009,"genre":"Weltmusik","thumbnail":"http://172.31.8.13:8090/vfs/special://masterprofile/Thumbnails/Music/c/c0e6d961.tbn","title":"Varnensko horo"},{"label":"03. Electric Guest - This Head I Hold","artist":"Electric Guest","album":"American Daydream - EP","year":2012,"genre":"Alternative","thumbnail":"http://172.31.8.13:8090/vfs/special://masterprofile/Thumbnails/Music/7/709dacee.tbn","title":"This Head I Hold"},{"label":"02. Peaches - AA XXX","artist":"Peaches","album":"The Teaches of Peaches","year":2000,"genre":"Electronic","thumbnail":"http://172.31.8.13:8090/vfs/special://masterprofile/Thumbnails/Music/f/f2344293.tbn","title":"AA XXX"}]'
+
+
+
+
+# def onOpen(ws)
+#   #clients.push(ws)
+#   #ws.send '
   
-end
+# end
 
-def sendPlaylist(clients)
-  clients.each do |ws|
-    ws.send('{"test":5}')
-  end
-end
+# def sendPlaylist(clients)
+#   clients.each do |ws|
+#     ws.send('{"test":5}')
+#   end
+# end
 
-EventMachine::WebSocket.start(:host => "172.31.8.50", :port => 8080) do |ws|
-  puts "start ws-server on port 8080"
+# def addTrack()
+
+# end
+
+# def pub
+#   @i += 1
+#   old = @playlist.first[:title].split("**").first
+#   @playlist.first[:title] = old + "**" + @i.to_s
+#   @channel.push(@playlist)
+# end
+
+# def on_msg(msg)
+#   puts "msg: #{msg}"
+# end
+
+EventMachine.run {
+  
+  puts "start ws-server #{@host} on port #{@port}"
   @channel = EM::Channel.new
- 
-  ws.onopen    { 
-    sid = @channel.subscribe { |msg| 
-      puts "sub"
-      ws.send msg.to_json 
-    }
-    
-    #@channel.push "#{sid} connected!"
-
-    ws.onmessage { |msg|
-      #@channel.push "<#{sid}>: #{msg}"
-    }
-
-    ws.onclose {
-      @channel.unsubscribe(sid)
-    }
-  }
-  ws.onclose   { puts "WebSocket closed" }
-
 
   EventMachine::PeriodicTimer.new(5) do
-    puts "push"
     @channel.push(@playlist)
   end
-      
-end
+
+  EventMachine::WebSocket.start(:host => @host, :port => @port) do |ws|
+    
+    ws.onopen    { 
+      sid = @channel.subscribe { |msg| 
+        ws.send msg.to_json 
+      }
+      puts "connected: #{sid}"
+
+      ws.onmessage { |msg| 
+        puts "send: #{msg.to_json}"
+        @channel.push(msg) 
+      }
+
+      ws.onclose {
+        puts "diconnected: #{sid}"
+        @channel.unsubscribe(sid)
+      }
+    }
+    ws.onclose   { puts "WebSocket closed" }
+
+        
+  end
+}
+
+
 
--- spotify-app/hotify.js
+++ spotify-app/hotify.js
@@ -1,6 +1,7 @@
 var sp = getSpotifyApi(1);
 var models = sp.require('sp://import/scripts/api/models');
 var player = models.player;
+//var client = sp.require('client');
 
 // expots
 exports.init = init;
@@ -8,17 +9,25 @@ exports.init = init;
 // options
 var server = "172.31.8.50:8080";
 
+
 // predefine
 var ws, playlist;
 
 
-function init() {
-  playlist = new models.Playlist();
-	connect();
+function init() { 
+  // playlist = new models.Playlist();
+	
+  // client.init({
+  //   msg: 
+  // });
 
-  // listen for player changes
-  player.observe(models.EVENT.CHANGE, onPlayerChange);
+  // // listen for player changes
+  // player.observe(models.EVENT.CHANGE, onPlayerChange);
+  connect();
+}
 
+exports.send = function(msg) {
+  ws.send(msg);
 }
 
 function connect() {
@@ -27,13 +36,13 @@ function connect() {
   ws.onclose = function() { console.log("socket closed"); };
   ws.onopen = function() {
     console.log("connected");
-    ws.send({room: "default"});
+    //ws.send();
   };
 }
 
 function onMsg(event) {
   console.log("onMsg: ", event, JSON.parse(event.data));
-  displayPlaylist();
+  //displayPlaylist();
 }
 
 function onPlayerChange(event) {
@@ -43,27 +52,27 @@ function onPlayerChange(event) {
   // }
 }
 
-function search(query, cb) {
-  var search = new models.Search(query);
-  search.localResults = models.LOCALSEARCHRESULTS.APPEND;
+// function search(query, cb) {
+//   var search = new models.Search(query);
+//   search.localResults = models.LOCALSEARCHRESULTS.APPEND;
 
-  search.observe(models.EVENT.CHANGE, function() {
-    search.tracks.forEach(function(track) {
-      //console.log(track.name);
-      playlist.add(track);
-    });
+//   search.observe(models.EVENT.CHANGE, function() {
+//     search.tracks.forEach(function(track) {
+//       //console.log(track.name);
+//       playlist.add(track);
+//     });
 
-    console.log(player, search.tracks[0], playlist, playlist.get(0), playlist.get(2));
-    player.play(search.tracks[0], playlist);
+//     console.log(player, search.tracks[0], playlist, playlist.get(0), playlist.get(2));
+//     player.play(search.tracks[0], playlist);
 
-  });
+//   });
 
-  search.appendNext();
-}
+//   search.appendNext();
+// }
 
-function displayPlaylist(trackArray) {
+// function displayPlaylist(trackArray) {
 
-}
+// }
 
 
 
