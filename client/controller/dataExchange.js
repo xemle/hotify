4fc7c46ac3266a96e99dc7e35598d5f7fab83671
Tobias Sarnow <tobias.sarnow@gmx.de>
2012-06-16 15:47:52 +0200

    data connect

--- client/controller/dataExchange.js
+++ client/controller/dataExchange.js
@@ -2,6 +2,7 @@
 	$(document).ready(function(){
 		// open a websocket connection
 		getActualSongInformation = function() {
+			/*
 			$.get("./example/interpret.json", function(data) {
 				console.log(data);
 				var song = $.parseJSON(data);
@@ -9,6 +10,15 @@
 				songModel.album(song["album"]);
 				songModel.interpret(song["interpret"]);
 				songModel.genre(song["genre"]);
+			});
+			*/
+			$.get("http://172.31.8.13:8080/hotify-server/services/activesong/", function(song) {
+				console.log(song);
+				songModel.title(song["title"]);
+				songModel.album(song["album"]);
+				songModel.interpret(song["artist"]);
+				songModel.genre(song["genre"]);
+				songModel.thumbnail(song["thumbnail"]);
 			});
 		};
 	});
--- client/model/song.js
+++ client/model/song.js
@@ -8,7 +8,9 @@
 		    length: ko.observable("3.17"),
 		    genre: ko.observable("Rock"),
 		    album: ko.observable("All in one"),
-		    votes: ko.observable("199")
+		    votes: ko.observable("199"),
+			thumbnail: ko.observable("http://172.31.8.13:8091/vfs/special://masterprofile/Thumbnails/Music/a/ad1321ee.tbn")
+
 		};
 		
 		songModel.details = ko.computed(function() {
